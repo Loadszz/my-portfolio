29a1abee90c5d0c6a45075de80622c17e2e7f2d2
Loadszz <66636253+Loadszz@users.noreply.github.com>
2024-09-03 15:08:22 +0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/index.css'
+import '@/styles/style.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>My Portfolio</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
